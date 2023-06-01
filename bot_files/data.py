72105458f3bb2cdc3cdcf6e7a27990e3828d6d8f
Shivam Sayankar <dr.strange996@gmail.com>
2023-06-02 03:27:23 +0530

    all the files added to repo

---
+++ bot_files/data.py
@@ -0,0 +1,113 @@
+
+MENU = {
+
+    'Maggie' : {
+
+        'Plain maggie' : 30,
+        'Masala maggie' : 45,
+        'Butter Maggie' : 40,
+        'Schezwan maggie' : 40,
+        'Anda maggie' : 50,
+        'Anda schezwan maggie' : 60,
+        'Extra cheese' : 20,
+    },
+
+
+    'Pulao' : {
+
+        'Veg pulao' : 60,
+        'Paneer pulao' : 80,
+        'Mashroom pulao' : 100,
+        'Green peas'  : 60,
+        'Daal khichadi' : 60,
+        'Butter daal khichadi' : 70,
+        'Curd rice' : 80,
+        'Extra cheese' : 20,
+    },
+
+
+    'Ande ka funda' : {
+
+        'Plane omelette' :40,
+        'Bun omelette' :50,
+        'Bread omelette ':50,
+        'Half fry':40,
+        'French toast':50,
+        'Egg bhurji':50,
+        'Anda rice':60,
+        'Boiled fry':50,
+        'Boiled (2 peices)':20,
+        'Extra pav':10,
+        'Extra cheese':20,
+    },
+
+
+    'Paratha' : {
+        'Aaloo Paratha' : 50,
+        'Panir Paratha' : 70,
+        'Gobi Paratha' : 50,
+        'Methi Paratha' : 60,
+        'Green Peas Paratha' : 50,
+        'Mix Veg Paratha' : 70,
+        'Laccha Paratha' : 30,
+        'Cheese Paratha' : 60,
+        'Chole Bhature' : 100,
+    },
+
+
+    'Hot drink' : {
+        'Hot Coffee' : 15,
+        'Hot Chocolate' : 40,
+        'Hot Bornvita' : 35,
+        'Hot Milk' : 20,
+        'Hot Termeric Milk' : 35
+    },
+
+
+    'Cold drink' : {
+        'Cold coffee ':25,
+        'Cold bornvita':40,
+        'Thick coffee ':40,
+        'Lassi':30,
+        'Nimbu pani':20,
+        'Taak':25
+    }
+
+}
+
+
+'''
+MENU = {
+'Tea' : 10,
+'Coffee' : 15,
+'Pohe' : 15,
+'Upma' : 15,
+'Shira' : 20,
+'Vada Pav' : 15,
+'Samosa' : 15,
+'Bread Pattice' : 15,
+'Veg Burger' : 30,
+'Chicken Burger' : 55,
+'Dahi Samosa' : 30,
+'Dahi Vada' : 50,
+'Idli Sambar' : 30,
+'Udid Wada Sambar' : 35,
+'Sabudana Khichdi' : 30,
+'Misal Pav' : 50,
+'Extra Pav Pair' : 10,
+'Plain Dosa' : 25,
+'Plain Uthappa' : 40,
+'Onion Uthappa' : 40,
+'Tomato Uthappa' : 50,
+'Plain Maggie' : 30,
+'Masala Maggie' : 45,
+'Veg Pulav' : 60,
+'Paneer Pulav' : 80,
+'Dal Khichdi' : 60,
+'Dal Khichdi Tadka' : 80,
+'Aaloo Paratha' : 50,
+'Paneer Paratha' : 80,
+'Gobi Paratha' : 50,
+}
+
+'''
---
+++ bot_files/main.py
@@ -0,0 +1,116 @@
+import os
+import telebot
+from telebot import types
+from credentials import API_TOKEN
+from data import MENU
+
+# API_TOKEN = os.environ['BOT_TOKEN']
+
+CHAT_ID = 1021725608
+
+commands = ['start','hello', 'menu', 'help', 'get_price']
+
+help_text = '''
+Here are the commands that can help you place your order:
+    /start
+    /help
+    /menu
+    /get_price    
+
+'''
+
+canteen_bot = telebot.TeleBot(API_TOKEN)
+
+
+# menu_button = types.KeyboardButton('Menu')
+# help_button = types.KeyboardButton('Help')
+# keyboard = types.InlineKeyboardMarkup()
+# keyboard.add(menu_button)
+
+
+@canteen_bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    canteen_bot.reply_to(message, "Hello there! I'm here to help you out with your canteen order ")
+
+
+@canteen_bot.message_handler(commands=['help'])
+def help_command(message):
+    canteen_bot.reply_to(message, help_text)
+
+
+@canteen_bot.message_handler(commands=['menu'])
+def show_menu(message):
+    # button = types.InlineKeyboardButton('Pani puri', callback_data='Pani Puri')
+
+    # buttons = [types.InlineKeyboardButton(category, callback_data=category) for category in MENU]
+
+    keyboard = types.InlineKeyboardMarkup()
+
+    maggie_button = types.InlineKeyboardButton('Maggie', callback_data='Maggie')
+
+    pulao_button = types.InlineKeyboardButton('Pulao', callback_data='Pulao')
+
+    ande_ka_funda_button = types.InlineKeyboardButton('Ande ka funda', callback_data='Ande ka funda')
+
+    paratha_button = types.InlineKeyboardButton('Paratha', callback_data='Paratha')
+
+    hot_drink_button = types.InlineKeyboardButton('Hot drink', callback_data='Hot drink')
+
+    cold_drink_button = types.InlineKeyboardButton('Cold drink', callback_data='Cold drink')
+    
+    keyboard.add(maggie_button, pulao_button,) 
+    keyboard.add(ande_ka_funda_button, paratha_button,)
+    keyboard.add(hot_drink_button, cold_drink_button)
+
+    # canteen_bot.send_message(chat_id=CHAT_ID, text='Here is our menu 👇', reply_markup=keyboard)
+    canteen_bot.reply_to(message, text='🔻 Here are the categories in our menu 🔻', reply_markup=keyboard)    
+
+
+
+@canteen_bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    if message.text not in commands:
+        canteen_bot.reply_to(message, f'Invalid command')
+        canteen_bot.reply_to(message, f'{help_text}')
+
+    # canteen_bot.reply_to(message, message.text)
+
+
+@canteen_bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    # Check if the callback is from an inline button
+    if call.message:
+        # Check the data of the button that was pressed
+        if call.data in MENU:
+            # button_handler(call.message)
+            canteen_bot.answer_callback_query(callback_query_id=call.id)
+            print(call.data)
+
+            # def show_submenu(category):
+            new_keyboard = types.InlineKeyboardMarkup()
+            buttons = [types.InlineKeyboardButton(f" {item} | ₹{MENU[call.data][item]}/- ", callback_data=item) for item in MENU[call.data] ]
+
+            # for item in MENU[call.data]:
+            #     types.InlineKeyboardButton(MENU[call.data][item], callback_data=MENU[call.data][item])
+            #     # print(list(MENU[call.data].keys()))
+            #     print(item)
+
+            for button in buttons:
+                new_keyboard.add(button) 
+
+            canteen_bot.reply_to(call.message, text=f'😋 Items in the category: {call.data} 😋', reply_markup=new_keyboard) 
+
+
+            callback_handler(call.data)
+
+            # show_submenu(call.data)
+            
+
+@canteen_bot.callback_query_handler(func=lambda call: True)
+def final_order_handler(call):
+    print(call.data)
+            
+
+
+
+canteen_bot.infinity_polling()
