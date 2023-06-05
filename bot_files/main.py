5617c08eb288edd552c3d24e20f92d1f916cbc46
Shivam Sayankar <dr.strange996@gmail.com>
2023-06-06 01:22:14 +0530

    fixed the -add more items- int error

--- bot_files/main.py
+++ bot_files/main.py
@@ -11,7 +11,24 @@ canteen_bot = telebot.TeleBot(API_TOKEN)
 current_category = ''
 item_selected = ''
 user_order_details = {}
+user_message = ''
 order_placed = False
+
+help_text = '''
+Here are the commands that can help you place your order:
+    /start
+    /help
+    /menu
+contact @NoobMaster_31 for issues or suggestions
+'''
+
+@canteen_bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    canteen_bot.reply_to(message, "Hello there! I'm here to help you out with your canteen order ")
+
+@canteen_bot.message_handler(commands=['help'])
+def help_command(message):
+    canteen_bot.reply_to(message, help_text)
 
 @canteen_bot.message_handler(commands=['menu'])
 def show_menu_categories(message):
@@ -41,6 +58,7 @@ def show_menu_categories(message):
 def show_menu(message):
 
     global item_selected, user_order_details
+    #user_message
 
     user_message = message.text.strip('🍜🍛🍳 🫓☕️🍹')
 
@@ -60,23 +78,23 @@ def show_menu(message):
 
         items_markup.add(*items)
 
-        canteen_bot.reply_to(message, text='🔻 Here are the categories in our menu 🔻', reply_markup=items_markup)
+        canteen_bot.reply_to(message, text=f'😋 Items in the category: {user_message} 😋', reply_markup=items_markup)
     
     elif user_message in COMPLETE_MENU:
 
         item_selected = user_message
 
-        canteen_bot.reply_to(message, text=f'{user_message} costs ₹{COMPLETE_MENU[user_message]}/-')
+        canteen_bot.reply_to(message, text=f'{item_selected} costs ₹{COMPLETE_MENU[item_selected]}/-')
 
         items = [
-            types.KeyboardButton(f'{i}') for i in range(1, 11)
+            types.KeyboardButton(f'{num}') for num in range(1, 11)
         ]
 
         items_markup.add(*items)
         canteen_bot.reply_to(message, text=f'How many {user_message}s would you like to order?', reply_markup=items_markup)
 
-    elif user_message.isnumeric:
-
+    elif user_message.isnumeric():
+        # print(f'user_message is {user_message} and its type is {type(user_message)}')
         num_of_items = int(user_message)
 
         print(f'do you want to order {user_message} {item_selected}s?')
@@ -104,17 +122,6 @@ def show_menu(message):
 
         print(f"{num_of_items} * {COMPLETE_MENU[item_selected]} {num_of_items*COMPLETE_MENU[item_selected]}")
         
-        '''with open('order_details.json', 'a+') as file:
-            try:
-                # data = json.load(file)
-                # print(data)
-                json.dump(user_order_details, file, indent=4)
-                data = json.load(file)
-                data.update(user_order_details)
-
-            except Exception as e:
-                print(e)
-'''
 
         try:
             with open("order_details.json", "r") as data_file:
@@ -136,13 +143,26 @@ def show_menu(message):
             pass
 
 
+        # user_message = str(user_message)
+
     elif user_message == 'Add more items':
-        show_menu(message)
+        # show_menu(message)
+
+        items = [
+            types.KeyboardButton(f'{item}') for item in MENU
+        ]
+
+        items_markup.add(*items)
+        canteen_bot.reply_to(message, text='🔻 Here are the categories in our menu 🔻', reply_markup=items_markup)
+    
         pass
 
     elif user_message == 'This is my final order':
         print(f'your final bill is:\
               {num_of_items}')
+        
+    else:
+        canteen_bot.reply_to(message, text='Invalid command')
 
 
 
